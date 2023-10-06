2941e2301d8addc6313c27ab0ca70d038dc16954
cyberdak <hyperdak@gmail.com>
2023-10-06 12:08:50 +0800

    Delete target directory
    delete target

--- target/generated-sources/annotations/com/moyucoding/test/jmh_generated/TypeConvTest_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.moyucoding.test.jmh_generated;
-public class TypeConvTest_jmhType extends TypeConvTest_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/com/moyucoding/test2/jmh_generated/MapTest_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.moyucoding.test2.jmh_generated;
-public class MapTest_jmhType extends MapTest_jmhType_B3 {
-}
-
